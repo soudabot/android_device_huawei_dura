4c7ba5e3cb03000526f8d35a2854938e3b6ea677
Souda <52483168+soudabot@users.noreply.github.com>
2023-05-29 21:27:20 -0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,3 +14,21 @@ TARGET_NO_BOOTLOADER := true
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32S1,32S1 product.type=normal androidboot.selinux=permisive 
+# Twrp and aosp should use the same cmdline but im not sure so it may change in future
+
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage-dtb
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x05000000
+BOARD_KERNEL_TAGS_OFFSET := 0x04000000
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+# BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_SOURCE := kernel/huawei/dura
+TARGET_KERNEL_CONFIG := dura_defconfig
+BOARD_MTKBOOTIMG_ARGS := --pagesize $(BOARD_KERNEL_PAGESIZE) --base $(BOARD_KERNEL_BASE) --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
